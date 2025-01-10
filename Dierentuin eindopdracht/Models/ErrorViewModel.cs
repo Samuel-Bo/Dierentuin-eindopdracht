6bb51bc1323c9f81258c4380ed9c6a60dbb75f7f
Samuel-Bo <s1200771@student.windesheim.nl>
2025-01-10 13:38:38 +0100

    Add project files.

---
+++ Dierentuin eindopdracht/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Dierentuin_eindopdracht.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
